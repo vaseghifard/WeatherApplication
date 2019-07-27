7baec401240f2fa730de9f75b3147413db5a04a5
Lenovo <z.vaseghifard1992@gmail.com>
2019-07-28 01:03:13 +0430

    Initial commit

---
+++ app/src/main/java/com/github/vaseghifard/weatherapplication/utils/MyEditText.java
@@ -0,0 +1,15 @@
+package com.github.vaseghifard.weatherapplication.utils;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+
+public class MyEditText extends AppCompatEditText {
+    public MyEditText(Context context) {
+        super(context);
+    }
+
+    public MyEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
